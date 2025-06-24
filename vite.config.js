d587516812d952d848935ed0682361b080593b06
emtun12 <vohoanghthien1709@gmail.com>
2025-06-24 09:58:34 +0700

    deploy

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,11 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/ThienVo-Portfolio",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+
     },
   },
 })
